621d1467137140b8119d08c11fc78b258a849833
Bruno Rossato de Araujo <bruno.araujo@ibm.com>
2019-07-18 14:08:58 -0300

    Fixing a conflict

--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import './menu.css'
 
 export default props => (
-  <nav className='navbar navbar-inverse bg-inverse'>
-    <div className='container'>
-      <div className='navbar-header'>
-        <a className='navbar-brand' href='#'>
-          <i className='fa fa-calendar-check-o'></i> TodoApp
+  <nav className="navbar navbar-inverse bg-inverse">
+    <div className="container">
+      <div className="navbar-header">
+        <a className="navbar-brand" href="#">
+          <i className="fa fa-calendar-check-o">ToDoApp</i>
         </a>
       </div>
-
-      <div id='navbar' className='navbar-collapse collapse'>
+      <div id="navbar" className="navbar-collapse collapse">
         <ul className="nav navbar-nav navbar-left">
           <li><a href='#/todos'>Tarefas</a></li>
           <li><a href='#/about'>Sobre</a></li>
