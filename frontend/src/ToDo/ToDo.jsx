68dabf749285d381ba994226376fedbee22f9348
Bruno Rossato <brunorossato@diogos-mbp-2.br.ibm.com>
2019-07-23 17:32:50 -0300

    button plus now is able to get the value inside the input and save in the mongo DB

--- frontend/src/ToDo/ToDo.jsx
+++ frontend/src/ToDo/ToDo.jsx
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-import Header from '../template/pageheader'
+import Header from '../template/pageheader';
+import axios from 'axios';
 import Form from '../todo/todoform';
-import List from '../todo/todolist'
+import List from '../todo/todolist';
+
+const URL = 'http://localhost:3003/api/todos'
+
 export default class Todo extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = { description: '' , list:[] }
+    this.handlleAdd = this.handlleAdd.bind(this)
+    this.handlleChange = this.handlleChange.bind(this)
+  }
+
+  handlleChange(e){
+    this.setState({...this.state, description: e.target.value })
+  }
+
+  handlleAdd(){
+    const description = this.state.description
+    axios.post(URL, { description })
+      .then(resp=> console.log('funcionouw'))
+  }
+
   render() {
     return (
       <div>
         <Header name='Tarefas' small='Cadastro'/>
-        <Form/>
+        <Form description={this.state.description}
+        handlleChange={this.handlleChange}
+        handlleAdd={this.handlleAdd}/>
         <List/>
       </div>
     );
--- frontend/src/ToDo/todoform.jsx
+++ frontend/src/ToDo/todoform.jsx
@@ -5,10 +5,12 @@ import Button from '../template/iconebutton'
 export default props =>(
   <div role='form' className='todoForm'>
       <Grid cols='12 9 10 '>
-        <input id='description' className='form-control' placeholder='Adcione uma tarefa'></input>
+        <input id='description' className='form-control' placeholder='Adcione uma tarefa'
+        onChange={props.handlleChange}
+        value={props.description}></input>
       </Grid>
       <Grid cols='12 3 2'>
-        <Button style='primary' icon='plus' hide={true}/>
+        <Button style='primary' icon='plus' onClick={props.handlleAdd}/>
       </Grid>
   </div>
 )
