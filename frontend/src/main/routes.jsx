65cc218b92ab48f6178e6da803bfd70427654f6d
eubrunorossato <bruno_facul@outlook.com>
2019-08-02 01:08:07 -0300

    trying to set cookies

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Router, Route, Redirect, hashHistory } from 'react-router';
+import decode from 'jwt-decode';
 import Todo from '../todo/todo';
 import About from '../about/about';
+import login from '../ToDo/login'
+
+const checkout = () => {
+  const token = localStorage.getItem('token');
+  const refreshToken = localStorage.getItem('refreshToken');
+  if(!token || !refreshToken) {
+    return false;
+  }
+
+  try {
+    const { exp } = decode(refreshToken);
+    console.log(exp)
+    console.log(new Date().getTime())
+
+    if(exp < new Date().getTime()){
+      return false;
+    }
+  } catch(e){
+    return false;
+  }
+  return true;
+}
+
+const AuthRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} component ={props => (
+    checkout() ? (
+      <Component {...props}/>
+    ) : (
+      <Redirect to={{ pathname: '/about' }}/>
+    )
+  )} />
+)
 
 export default props => (
   <Router history={ hashHistory }>
     <Route path='/todos' component={ Todo } />
     <Route path='/about' component={ About } />
+    <AuthRoute path='/auth' component={ login } />
     <Redirect from='*' to='/todos' />
   </Router>
 )
