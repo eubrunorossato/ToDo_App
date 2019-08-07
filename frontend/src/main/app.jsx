58d5e72e26f47b65141d8986097d7518c56e1e0d
Bruno Rossato de Araujo <bruno.araujo@ibm.com>
2019-08-07 16:57:02 -0300

    code refactor

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,56 +3,45 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 import '../Style/custom.css'
 
 import React, { Component } from 'react';
-// import login from '../Components/login';
-// import Menu from '../Components/menuClass'
-// import Index from '../Routes/index';
-// import Login from '../Routes/login';
-import { BrowserRouter, Switch, Route, HashRouter } from 'react-router-dom'
+import { Switch, Route, HashRouter } from 'react-router-dom';
 import Index from '../Components/ToDo';
 import Login from '../Components/login';
+import Menu from '../template/menu';
 import { createHashHistory } from 'history'
 const history = createHashHistory()
 
 
 export default class App extends Component {
-
   constructor(props) {
     super(props);
     this.Authentication = this.Authentication.bind(this);
-
   };
 
-
-//   // exportMenu() {
-//   //   return localStorage.getItem('token') && localStorage.getItem('refreshToken') ? <Menu register='' tasks='Tasks' /> : <Menu register='Register' tasks='' />;
-//   // };
-
-//   // exportBody() {
-//   //   return localStorage.getItem('token') && localStorage.getItem('refreshToken') ? <Index /> : <Login/>;
-//   // };
-
-  Authentication (props) {
-    if(localStorage.getItem('token') && localStorage.getItem('refreshToken')){
-        console.log("index")
+  Authentication() {
+    if (!localStorage.getItem('token') && localStorage.getItem('refreshToken')) {
+      history.push("/homePage")
+      return true
+    } else {
+      history.push("/login")
+      return false;
     }
-    history.push('/homePage')
   }
-
-
 
   render() {
     return (
       <div className='container'>
-      <HashRouter>
-        <Switch>
-            <Route exact path ='/login' component={Login}/>
+        <div className='container'>
+          <Menu register={this.Authentication() ? null : 'Register'} tasks={this.Authentication() ? 'Tasks' : null} />
+        </div>
+        <HashRouter>
+          <Switch>
+            <Route exact path='/login' component={Login} />
             <Route path='/' component={this.Authentication()}>
-              <Route path='/homePage' component={Index}/>
+              <Route path='/homePage' component={Index} />
             </Route>
           </Switch>
-      </HashRouter>
+        </HashRouter>
       </div>
     )
   }
-
 }
